9220d65f946348f92a644aca72f7b082d6e58ed7
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2017-03-14 12:10:13 -0700

    history-items UI

--- src/components/HistoryItem/HistoryItem.js
+++ src/components/HistoryItem/HistoryItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './HistoryItem.css';
     
-class HistoryItem extends Component {
-    render() {
-        return (
-            <div className="history-item">
-                stuff
-            </div>
-        );
-    }
-}
+const HistoryItem = () => (
+    <div className="history-item">
+        <div className="history-item-left">
+            <div className="history-item-name">Danny</div>
+            <div className="history-item-desc">Malibu wine Safari</div>
+        </div>
+        <div className="history-item-right">
+            <div className="history-item-date">Sun, March 12</div>
+            <div className="history-item-amount">-$130</div>
+        </div>
+    </div>
+);
 
 export default HistoryItem; 
     
